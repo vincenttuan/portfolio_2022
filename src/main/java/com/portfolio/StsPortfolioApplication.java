d56c88592ab805aa43509674057c4fb402a5102a
vincenttuan <vincentjava@yahoo.com.tw>
2022-03-24 11:35:05 +0800

    portfolio

---
+++ src/main/java/com/portfolio/StsPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsPortfolioApplication.class, args);
+	}
+
+}
