9ecea90bc7205d8ad24dc7b9e80e85f821b767a5
vincenttuan <vincentjava@yahoo.com.tw>
2022-03-26 17:25:12 +0800

    p

--- src/main/java/com/portfolio/entity/Investor.java
+++ src/main/java/com/portfolio/entity/Investor.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -35,7 +36,7 @@ public class Investor {
 	@JsonIgnoreProperties("investor")
 	private Set<Watch> watchs;
 	
-	@OneToMany
+	@OneToMany(mappedBy = "investor")
 	@JsonIgnoreProperties("investor")
 	private Set<Portfolio> portfolios;
 	
--- src/main/java/com/portfolio/filter/LoginFilter.java
+++ src/main/java/com/portfolio/filter/LoginFilter.java
@@ -9,22 +9,44 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.portfolio.entity.Investor;
+import com.portfolio.repository.InvestorRepository;
+
 /* 要將 StsPortfolioApplication.java 配置加上 @ServletComponentScan */
 @WebFilter("/*")
 public class LoginFilter extends BaseFilter {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Autowired
+	private InvestorRepository investorRepository;
+	
 	@Override
 	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		HttpSession session = request.getSession();
 		System.out.println("LoginFilter");
+		if(request.getParameter("investor_id") != null) {
+			// 根據 investor_id 進行登入
+			int id = Integer.parseInt(request.getParameter("investor_id"));
+			Investor investor = investorRepository.findById(id).get();
+			// 將 investor 物件資料存入 session
+			session.setAttribute("investor_username", investor.getUsername());
+			session.setAttribute("investor_id", investor.getId());
+			session.setAttribute("watch_id", investor.getWatchs().iterator().next().getId());
+			session.setAttribute("investor", investor);
+			// 將 session 資料印出
+			System.out.println("session investor = " + investor);
+		}
+		/*
 		// 測試資料
 		session.setAttribute("investor_username", "User");
 		session.setAttribute("investor_id", 1);
 		session.setAttribute("watch_id", 1);
 		session.setAttribute("investor", null);
+		*/
 		chain.doFilter(request, response);
 	}
 	
