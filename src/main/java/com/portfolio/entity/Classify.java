4da5e6b1b07965513da078346ef744bd08040e3d
vincenttuan <vincentjava@yahoo.com.tw>
2022-03-26 15:00:48 +0800

    p

--- src/main/java/com/portfolio/entity/Classify.java
+++ src/main/java/com/portfolio/entity/Classify.java
@@ -2,6 +2,7 @@ package com.portfolio.entity;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
